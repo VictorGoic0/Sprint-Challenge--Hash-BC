f08560df97fd04617eb523db8c3e6a1096144c06
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-07-12 11:43:10 -0400

    Example 1 hash table problem completed.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,11 +8,15 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+    for i in range(length):
+        if hash_table_retrieve(ht, limit-weights[i]):
+            index_1 = i
+            index_2 = hash_table_retrieve(ht, limit-weights[i])
+            if index_1 > index_2:
+                return (index_1, index_2)
+            return (index_2, index_1)
     return None
 
 
